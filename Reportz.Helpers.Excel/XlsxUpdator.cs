b8d6855fc2a14c45e1d46d7f4e55aeba29b8709e
LazyTarget <peter.aslund@hotmail.com>
2017-01-22 00:54:25 +0100

    Exporting to Excel (xlsx)

---
+++ Reportz.Helpers.Excel/XlsxUpdator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using OfficeOpenXml;
+using Reportz.Scripting.Classes;
+using Reportz.Scripting.Interfaces;
+
+namespace Reportz.Helpers.Excel
+{
+    public class XlsxUpdator : IExecutable
+    {
+        public XlsxUpdator()
+        {
+            
+        }
+
+        public IExecutableResult Execute(IExecutableArgs args)
+        {
+            ExcelPackage pkg = null;
+            try
+            {
+                var doc = args?.Arguments?.FirstOrDefault(x => x.Key == "document")?.Value;
+                if (doc is ExcelPackage)
+                    doc = ((ExcelPackage) doc).Workbook;
+                if (doc is ExcelWorkbook)
+                    doc = ((ExcelWorkbook) doc).Worksheets.FirstOrDefault();
+                var ws = doc as ExcelWorksheet;
+                if (ws == null)
+                {
+                    var fileInfo = new FileInfo("ExcelDoc.xlsx");
+                    pkg = new ExcelPackage(fileInfo);
+                    ws = pkg.Workbook.Worksheets.Add("Sheet1");
+                    //ws = new ExcelWorksheet();
+                }
+
+
+                var dt = args?.Arguments?.FirstOrDefault(x => x.Key == "data")?.Value;
+                dt = (dt as IExecutableResult)?.Result ?? dt;
+                var dataTable = dt is DataTable
+                    ? (DataTable) dt
+                    : null;
+                if (dataTable != null)
+                {
+                    for (var y = 0; y < dataTable.Columns.Count; y++)
+                    {
+                        for (var x = 0; x < dataTable.Rows.Count; x++)
+                        {
+                            var value = dataTable.Rows[x][y];
+                            //ws.Cells[y, x].Value = value;
+                            ws.SetValue(x + 1, y + 1, value);
+                        }
+                    }
+                }
+
+                // todo: apply dataSet to Worksheet
+                object res = null;
+
+                var result = new ExecutableResult
+                {
+                    Result = res,
+                };
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                pkg?.Save();
+            }
+        }
+    }
+}
---
+++ Reportz.Plugin.ProjectPlace/JsonToDataTableFormatter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Reportz.Scripting.Classes;
+using Reportz.Scripting.Interfaces;
+
+namespace Reportz.Plugin.ProjectPlace
+{
+    public class JsonToDataTableFormatter : IExecutable
+    {
+        public JsonToDataTableFormatter()
+        {
+            
+        }
+
+        public IExecutableResult Execute(IExecutableArgs args)
+        {
+            try
+            {
+                var json = args?.Arguments?.FirstOrDefault(x => x.Key == "json")?.Value;
+                var jsonTkn = json is JToken
+                    ? (JToken) json
+                    : JToken.Parse(json?.ToString());
+                
+                var dataTable = new DataTable();
+                ParseToken(jsonTkn, dataTable, null, null);
+
+                var headerRow = dataTable.NewRow();
+                dataTable.Rows.InsertAt(headerRow, 0);
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    headerRow[column] = column.ColumnName;
+                }
+
+                var result = new ExecutableResult
+                {
+                    Result = dataTable,
+                };
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void ParseToken(JToken token, DataTable dataTable, DataRow currentRow, string[] currentPrefixes)
+        {
+            var objects = new List<JObject>();
+            if (token.Type == JTokenType.Array)
+            {
+                foreach (var tkn in token)
+                {
+                    var obj = tkn as JObject;
+                    if (obj != null)
+                        objects.Add(obj);
+                }
+            }
+            else if (token.Type == JTokenType.Object)
+            {
+                objects.Add((JObject) token);
+            }
+
+            
+            foreach (var obj in objects)
+            {
+                var row = currentRow;
+                if (row == null)
+                {
+                    row = dataTable.NewRow();
+                    dataTable.Rows.Add(row);
+                }
+
+                var props = obj.Properties();
+                foreach (var prop in props)
+                {
+                    var prefixes = currentPrefixes ?? new string[0];
+                    prefixes = prefixes.Concat(new [] {prop.Name}).ToArray();
+
+                    var columnName = string.Join(".", prefixes);
+                    var column = dataTable.Columns[columnName] ?? dataTable.Columns.Add(columnName);
+                    
+                    if (prop.Type == JTokenType.Null)
+                    {
+                        row[column] = null;
+                    }
+                    else if (prop.Value is JValue)
+                    {
+                        row[column] = prop.Value.ToString();
+                    }
+                    else
+                    {
+                        ParseToken(prop.Value, dataTable, row, prefixes);
+                    }
+                }
+            }
+        }
+    }
+}
