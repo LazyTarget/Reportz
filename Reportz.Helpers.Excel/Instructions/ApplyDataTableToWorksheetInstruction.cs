c8cf21888b2a160adae2456ffb83171cba331851
LazyTarget <peter.aslund@hotmail.com>
2017-02-22 20:06:16 +0100

    Working on datatable-to-worksheet instruction

---
+++ Reportz.Helpers.Excel/Instructions/ApplyDataTableToWorksheetInstruction.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Xml.Linq;
+using OfficeOpenXml;
+using Reportz.Helpers.Excel.Attributes;
+using Reportz.Scripting.Interfaces;
+
+namespace Reportz.Helpers.Excel.Instructions
+{
+    [XlsxScriptElementAlias("datatable-to-worksheet")]
+    [XlsxScriptElementAlias("apply-datatable-to-worksheet")]
+    public class ApplyDataTableToWorksheetInstruction : XlsxInstructionBase
+    {
+        private XElement _element;
+        private IScriptParser _parser;
+
+        public ApplyDataTableToWorksheetInstruction()
+        {
+            
+        }
+
+        public override void Configure(IScriptParser parser, XElement element)
+        {
+            _parser = parser;
+            _element = element;
+        }
+
+        public override object Execute(ExcelPackage package, ExcelWorksheet worksheet, IExecutableArgs args)
+        {
+            var dataElem = _element.Element("data");
+            if (dataElem == null || dataElem.IsEmpty)
+                throw new InvalidOperationException();
+
+            object data;
+            if (dataElem.HasElements)
+            {
+                var rootDataElem = dataElem.Elements().First();
+                data = _parser.InstantiateElement(rootDataElem);
+            }
+            else
+            {
+                data = _parser.EvaluateExpression(args.Scope, dataElem.Value);
+            }
+
+            var dataTable = data as DataTable;
+            if (dataTable == null)
+            {
+                // todo: keep for backwards compatibility?
+                var dt = args?.Arguments?.FirstOrDefault(x => x.Key == "data")?.Value;
+                dt = (dt as IExecutableResult)?.Result ?? dt;
+                dataTable = dt is DataTable
+                    ? (DataTable)dt
+                    : null;
+
+                if (dataTable == null)
+                    throw new InvalidOperationException("Invalid 'dataTable' value");
+            }
+
+
+            ExcelWorksheet ws = worksheet;
+            if (dataTable != null)
+            {
+                var hasMaps = false;
+                if (hasMaps)
+                {
+
+                }
+                else
+                {
+                    for (var y = 0; y < dataTable.Columns.Count; y++)
+                    {
+                        for (var x = 0; x < dataTable.Rows.Count; x++)
+                        {
+                            // apply dataTable to Worksheet
+
+                            var value = dataTable.Rows[x][y];
+                            //ws.Cells[y, x].Value = value;
+                            ws.SetValue(x + 1, y + 1, value);
+                        }
+                    }
+                }
+            }
+
+            object res = null;
+            return res;
+        }
+    }
+}
---
+++ Reportz.Helpers.Excel/Instructions/DeleteWorksheetInstruction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using OfficeOpenXml;
+using Reportz.Helpers.Excel.Attributes;
+using Reportz.Scripting.Interfaces;
+
+namespace Reportz.Helpers.Excel.Instructions
+{
+    [XlsxScriptElementAlias("delete-worksheet")]
+    public class DeleteWorksheetInstruction : XlsxInstructionBase
+    {
+        public DeleteWorksheetInstruction()
+        {
+            
+        }
+
+        public string SheetName { get; set; }
+
+
+        public override void Configure(IScriptParser parser, XElement element)
+        {
+            SheetName = element?.Attribute("sheetName")?.Value;
+        }
+
+        public override object Execute(ExcelPackage package, ExcelWorksheet worksheet, IExecutableArgs args)
+        {
+            ExcelWorksheet ws = null;
+            var sheetName = SheetName;
+            if (string.IsNullOrWhiteSpace(sheetName))
+            {
+                //throw new ArgumentException();
+                sheetName = worksheet?.Name;
+            }
+
+            if (package.Workbook.Worksheets.Any(x => x.Name == sheetName))
+            {
+                package.Workbook.Worksheets.Delete(sheetName);
+                ws = package.Workbook.Worksheets.FirstOrDefault();
+                if (ws == null)
+                {
+
+                }
+            }
+            else
+            {
+                throw new Exception($"Could not find worksheet to remove. SheetName: {sheetName}. ");
+            }
+            return ws;
+        }
+    }
+}
